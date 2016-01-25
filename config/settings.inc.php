17e8565d6fca49b039fc9aad4df30866fa07e7ee
SoundManPresta <georgescu_costin@yahoo.com>
2016-01-25 10:51:31 +0200

    First commit

---
+++ config/settings.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_PS_CACHING_SYSTEM_', 'CacheFs');
+define('_PS_CACHE_ENABLED_', '0');
+define('_DB_NAME_', 'prestadeveloper');
+define('_MYSQL_ENGINE_', 'MyISAM');
+define('_DB_SERVER_', '127.4.25.130');
+define('_DB_USER_', 'adminXHahI42');
+define('_DB_PREFIX_', 'ps_');
+define('_DB_PASSWD_', 'du4fn3wyERjv');
+define('_COOKIE_KEY_', 'igvkx8qlsovp5kg56xqf5moncil0mesljotminohs1pppgycus47anqe');
+define('_COOKIE_IV_', 'ibxkzkdt');
+define('_PS_CREATION_DATE_', '2014-01-21');
+define('_RIJNDAEL_KEY_', '28N2FLDLdda9bxV4b7xoq4CBOeJ0tJm8');
+define('_RIJNDAEL_IV_', '4+ENPiCEEao031HL+GpIyA==');
+define('_PS_VERSION_', '1.6.1.4');
